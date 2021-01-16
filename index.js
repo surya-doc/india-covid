92a2634631594b63d5259bf1bbda81254a8463f3
surya-doc <suryadanabnk@gmail.com>
2021-01-16 20:49:20 +0530

    F m

--- index.js
+++ index.js
@@ -14,116 +14,128 @@ app.use(express.static("public"));
 
 app.set('view engine', 'ejs');
 
-var reqTitle = "";
-var optionsDistrictWise = {
-  'method': 'GET',
-  'url': "https://api.covid19india.org/v4/data.json",
-  'headers': {
-  }
-};
 
-var disArr = [];
-var indianArr = [];
-request(optionsDistrictWise, function (error, response) {
-    if (error) throw new Error(error);
-    var sh = JSON.parse(response.body);
-    for(var val in sh) {
-      
-      var state = {
-        stateCode: "",
-        confirmed: "",
-        deceased: "",
-        recovered: "",
-        tested: "",
-        stateName: "",
-        dis: []
-      }
-      state.stateCode = val;
-        for(var tot in sh[val]) {
-          if(tot === 'total'){
-            state.confirmed = sh[val][tot]['confirmed'].toLocaleString();
-            // console.log(state.confirmed);
-            state.deceased = Number(sh[val][tot]['deceased']).toLocaleString();
-            state.recovered = Number(sh[val][tot]['recovered']).toLocaleString();
-            state.tested = Number(sh[val][tot]['tested']).toLocaleString();
-          }
-          if(tot === 'districts'){
-
-            for(var disName in sh[val][tot]){
-              var district = {
-                districtName: "",
-                disConfirmed: "",
-                disDeceased: "",
-                disRecovered: ""
-              }
-
-              district.districtName = disName;
-              for(var disData in sh[val][tot][disName]){
-                if(disData === 'total'){
-                  var info = sh[val][tot][disName][disData];
-                  district.disConfirmed = Number(info['confirmed']).toLocaleString();
-                  district.disDeceased = Number(info['deceased']).toLocaleString();
-                  district.disRecovered = Number(info['recovered']).toLocaleString();
-                }
-              }
-
-              state.dis.push(district);
-
-            }
-
-          }
-        }
-      
-      disArr.push(state);
-    }
-
-});
-
-
-var updatedTime = "";
 var request = require('request');
-var optionsStateWise = {
-  'method': 'GET',
-  'url': 'https://api.covid19india.org/data.json',
-  'headers': {
-  }
-};
-request(optionsStateWise, function (error, response) {
-  if (error) throw new Error(error);
-  const stateData = JSON.parse(response.body);
-  updatedTime = stateData.statewise[0].lastupdatedtime;
-  for(var val in stateData){
-      if(val === 'statewise'){
-          for(var stateName in stateData[val]){
-            for(var i=0; i < disArr.length; i++){
-              if(stateData[val][stateName]['statecode'] === disArr[i].stateCode){
-                disArr[i].stateName = stateData[val][stateName]['state'];
-              }
-            }
-          }
-      }
-      console.log(updatedTime);
-  }
-
-  var indianData = {
-    inActive: "",
-    inConfirmed: "",
-    inDeaths: "",
-    inRecovered: ""
-  }
-
-  indianData.inActive = Number(stateData.statewise[0].active).toLocaleString();
-  indianData.inConfirmed = Number(stateData.statewise[0].confirmed).toLocaleString();
-  indianData.inDeaths = Number(stateData.statewise[0].deaths).toLocaleString();
-  indianData.inRecovered = Number(stateData.statewise[0].recovered).toLocaleString();
-
-  indianArr.push(indianData);
-
-});
-
 
 app.get('/', (req, res) => {
-  res.render('home', {time: updatedTime, arr: disArr, indiArr: indianArr});
+
+  var reqTitle = "";
+  var updatedTime = "";
+  var disArr = [];
+  console.log(disArr);
+  var indianArr = [];
+  var optionsDistrictWise = {
+    'method': 'GET',
+    'url': "https://api.covid19india.org/v4/data.json",
+    'headers': {
+    }
+  };
+
+  request(optionsDistrictWise, function (error, response) {
+      if (error) throw new Error(error);
+      var sh = JSON.parse(response.body);
+      for(var val in sh) {
+        var state = {
+          stateCode: "",
+          confirmed: "",
+          deceased: "",
+          recovered: "",
+          tested: "",
+          stateName: "",
+          dis: []
+        }
+        state.stateCode = val;
+          for(var tot in sh[val]) {
+            if(tot === 'total'){
+              state.confirmed = sh[val][tot]['confirmed'].toLocaleString();
+              // console.log(state.confirmed);
+              state.deceased = Number(sh[val][tot]['deceased']).toLocaleString();
+              state.recovered = Number(sh[val][tot]['recovered']).toLocaleString();
+              state.tested = Number(sh[val][tot]['tested']).toLocaleString();
+            }
+            if(tot === 'districts'){
+  
+              for(var disName in sh[val][tot]){
+                var district = {
+                  districtName: "",
+                  disConfirmed: "",
+                  disDeceased: "",
+                  disRecovered: ""
+                }
+  
+                district.districtName = disName;
+                for(var disData in sh[val][tot][disName]){
+                  if(disData === 'total'){
+                    var info = sh[val][tot][disName][disData];
+                    district.disConfirmed = Number(info['confirmed']).toLocaleString();
+                    district.disDeceased = Number(info['deceased']).toLocaleString();
+                    district.disRecovered = Number(info['recovered']).toLocaleString();
+                  }
+                }
+  
+                state.dis.push(district);
+  
+              }
+  
+            }
+          }
+        disArr.push(state);
+      }
+  
+  });
+
+  
+
+  console.log(disArr);
+
+  
+  var optionsStateWise = {
+    'method': 'GET',
+    'url': 'https://api.covid19india.org/data.json',
+    'headers': {
+    }
+  };
+  request(optionsStateWise, function (error, response) {
+    if (error) throw new Error(error);
+    const stateData = JSON.parse(response.body);
+    updatedTime = stateData.statewise[0].lastupdatedtime;
+    for(var val in stateData){
+        if(val === 'statewise'){
+            for(var stateName in stateData[val]){
+              for(var i=0; i < disArr.length; i++){
+                if(stateData[val][stateName]['statecode'] === disArr[i].stateCode){
+                  disArr[i].stateName = stateData[val][stateName]['state'];
+                }
+              }
+            }
+        }
+    }
+  
+    var indianData = {
+      inActive: "",
+      inConfirmed: "",
+      inDeaths: "",
+      inRecovered: ""
+    }
+  
+    indianData.inActive = Number(stateData.statewise[0].active).toLocaleString();
+    indianData.inConfirmed = Number(stateData.statewise[0].confirmed).toLocaleString();
+    indianData.inDeaths = Number(stateData.statewise[0].deaths).toLocaleString();
+    indianData.inRecovered = Number(stateData.statewise[0].recovered).toLocaleString();
+  
+    indianArr.push(indianData);
+  
+  });
+
+  console.log(disArr, indianArr);
+  if(disArr.length == 0){
+    console.log('Empty!!');
+  }
+  else 
+  {
+    res.render('home', {time: updatedTime, arr: disArr, indiArr: indianArr});
+
+  }
 })
 
 app.post('/', (req, res) => {
